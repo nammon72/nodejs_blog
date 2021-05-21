4e825297c2bf57c3e2f441dcc42c418665350521
Nam Nguyen <namnh.vst@gmail.com>
2021-05-21 14:34:00 +0700

    install morgan

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
-  res.send('Hello World!')
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  return res.send(`
+    <h1>Hello World</h1>
+  `);
 })
 
 app.listen(port, () => {
